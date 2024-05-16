39f60a0d73db33f4c8c8702e5ce17db54432082e
库淑贞 <kushuzhen@eastmoney.com>
2024-05-16 13:11:56 +0800

    TransferImageUrl

---
+++ src/test/java/com/fourteen/springboottest/TransferImageUrl.java
@@ -0,0 +1,25 @@
+package com.fourteen.springboottest;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Base64;
+
+/**
+ * @author Fourteen_ksz
+ * @version 1.0
+ * @date 2024/5/16 10:49
+ */
+@SpringBootTest
+public class TransferImageUrl {
+
+    @Test
+    public void test() throws Exception {
+        String url = "https://mmbiz.qpic.cn/mmbiz_jpg/Ek8t172PVzNKxicNcDHuE2Q6MbQ1haicBLbEXJKmibaRX6HBsmvez5xUQGhb7oJOyiaIFjWVVWWA2kPgkJaTKdu80Q/0?wx_fmt=jpeg";
+        System.out.println(Base64.getEncoder().encodeToString(IOUtils.toByteArray(new URL(url))));
+    }
+
+}
